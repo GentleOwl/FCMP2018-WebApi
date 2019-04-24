d19e2c6616cd7db3afd3db03c6424ad4094f64fa
3paFiodarau2 <Aliaksandr_Fiodarau2@epam.com>
2019-04-24 20:42:39 +0300

    Add news

--- app.js
+++ app.js
@@ -1,35 +1,41 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const bodyParser = require('body-parser');
+const config = require('./config');
+const cors = require('cors');
 
-const indexRouter = require('./routes');
 const newsRouter = require('./routes/news');
+
+mongoose.connect(config.databaseUri, { useNewUrlParser: true });
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connection successful');
+});
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use('/news', newsRouter);
-
-app.use('/error', function (req, res, next) {
-    throw new Error('e-r-r-o-r');
-});
 
 app.use(function(err, req, res, next) {
     res.locals.message = err.message;
     res.locals.status = err.status || 500;
 
     res.status(err.status || 500);
-    res.render('error');
 });
 
 module.exports = app;
--- routes/news.js
+++ routes/news.js
@@ -2,24 +2,32 @@ const express = require('express');
 const newsService = require('../services/news-service');
 const router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.json(newsService.getNews());
+router.get('/', async (req, res, next) => {
+  const newsItems = await newsService.getNews();
+
+  res.json(newsItems);
 });
 
-router.get('/:id', function(req, res, next) {
-  res.json(newsService.getNewsById(+req.params.id));
+router.get('/:id', async (req, res, next) => {
+  const newsItem = await newsService.getNewsById(req.params.id);
+
+  res.json(newsItem);
 });
 
 router.delete('/:id', function(req, res, next) {
-  res.json(newsService.deleteNews(+req.params.id));
+  res.json(newsService.deleteNews(req.params.id));
 });
 
-router.put('/', function(req, res, next) {
-  res.json(newsService.addNews(req.body));
+router.put('/', async (req, res, next) => {
+  const newsItem = await newsService.addNews(req.body)
+
+  res.json(newsItem);
 });
 
-router.post('/', function(req, res, next) {
-  res.json(newsService.updateNews(req.body));
+router.post('/:id', async (req, res, next) => {
+  const newsItem = await newsService.updateNews(req.params.id, req.body)
+
+  res.json(newsItem);
 });
 
 module.exports = router;
